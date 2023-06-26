7ae1652946da9a4185b5f2b8db3bf5364b0dbe15
EG-AKANDE <emmanuel_akande@yahoo.com>
2023-06-25 22:37:53 -0700

    monty project codes

---
+++ nop.c
@@ -0,0 +1,15 @@
+#include "monty.h"
+
+/**
+  *f_nop- none
+  *@head: head
+  *@counter: line no
+  *Return: none ret
+ */
+
+void f_nop(stack_t **head, unsigned int counter)
+
+{
+	(void) counter;
+	(void) head;
+}
---
+++ stack.c
@@ -0,0 +1,16 @@
+#include "monty.h"
+
+/**
+ * f_stack - prints top
+ * @head: head
+ * @counter: line no
+ * Return: none ret
+*/
+
+void f_stack(stack_t **head, unsigned int counter)
+
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
